ae448980f8d1eb115a7dcd9822f44abe3dfb646b
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-02-15 22:00:28 +0100

    refactor: rename the boolean variables

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -3,29 +3,29 @@
 const user = {
   name: 'Diego Fernandes',
   height: 190,
-  hasTicket: true,
+  hasTicket: true
 }
 
-const necessaryHeight = 130
+const userHasRequiredHeight = 130
 
 const currentHour = new Date().getHours()
 
-const parkOpen = currentHour > 9 && currentHour < 18
+const isParkOpen = currentHour > 9 && currentHour < 18
 
-if (!parkOpen) {
+if (!isParkOpen) {
   throw new Error('O parque está fechado!')
 }
 
-const ticket = user.hasTicket
+const userHasTicket = user.hasTicket
 
-if (!ticket) {
+if (!userHasTicket) {
   throw new Error('O Diego não possui um bilhete para entrar no parque!')
 }
 
-const enterToy = user.height > necessaryHeight
+const doesTheUserCanPlayOnTheToy = user.height > userHasRequiredHeight
 
-if (!enterToy) {
+if (!doesTheUserCanPlayOnTheToy) {
   throw new Error('O Diego não pode entrar no brinquedo!')
-} 
+}
 
 console.log('O Diego se divertiu muito! :)')
