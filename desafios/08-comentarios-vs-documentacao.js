e67222968c4cd6464eeb8e78eaf218ef43d9c56c
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-02-21 22:18:29 +0100

    refactor: update challenge 8

--- desafios/08-comentarios-vs-documentacao.js
+++ desafios/08-comentarios-vs-documentacao.js
@@ -1,28 +1,26 @@
-// Essa função tem o objetivo de registrar um novo usuário no banco.
-async function register(data) {
-  // Desestrutura os dados recebidos.
+async function createNewUser(data) {
   const { email, name, avatar } = data
-
-  // Verifica se o avatar foi informado.
   if (!avatar) return { error: 'avatar is required' }
 
-  // Verifica se o nome foi informado.
-  if(!name) return { error: 'name is required' }
+  if (!name) return { error: 'name is required' }
 
-  // Essa função retorna o usuário a partir do email informado
-  const userMail = getUserByEmail(email)
+  const userByEmail = getUserByEmail(email)
 
-  // Verificação se o email informado já foi utilizado por algum usuário.
-  if (userMail) {
+  if (userByEmail) {
     return { error: 'email already used' }
   }
 
-  // Essa função realiza a conversão das imagens para JPG a fim de evitar erros de incompatibilidade.
-  // Mais informações na issue https://github.com/rocketseat-education/example-repository/issues/1
-  const avatar2 = convertImageToJPG(avatar)
+  /**
+   *  Essa função realiza a conversão das imagens para JPG a fim de evitar erros de incompatibilidade.
+   *  Mais informações na issue https://github.com/rocketseat-education/example-repository/issues/1
+   */
+  const convertAvatarInJpg = convertImageToJPG(avatar)
 
-  // Cria o usuário no banco de dados
-  const user = await createUser({ email, name, avatar: avatar2 })
+  const createNewUserResponse = await createUser({
+    email,
+    name,
+    avatar: convertAvatarInJpg
+  })
 
-  return { user }
+  return { createNewUserResponse }
 }
